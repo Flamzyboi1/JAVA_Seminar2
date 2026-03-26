35c7decdd881e5082f78783bde5f3e43f3868059
Flamzyboi1 <favourobidiaso@gmail.com>
2026-03-26 09:52:30 +0200

    changed student class to use person as a base class

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,11 +1,13 @@
 package model;
 
-public class Student {
+public class Student extends Person{
 	private String matriculaNumber;
 	
 	private String faculty;
 	private int birthYear;
 	private Country country;
+	
+	// Name,surName, passportNumber, is from person class
 	
 	//getters
 	public String getMatriculaNumber() {
@@ -67,25 +69,27 @@ public class Student {
 	
 	// no -arg constructor  
 	public Student() {
+		super();//
 		setmatriculaNumber("AB123456");
-		setName("Favour");
-		setsurName("Obidiaso");
+		//setName("Favour");
+		//setsurName("Obidiaso");
 		setfaculty("ITF");
 		setBirthYear(1999);
 		setcountry(Country.Spain);
-		setpassportNumber("SP09235");
+		//setpassportNumber("SP09235");
 	}
 	//arg-constructor
 	 public Student(String inputMatriculanumber,String inputName,
 	 String inputSurname, String inputFaculty,int inputBirthyear, Country inputCountry,
 	 String inputPassportnumber){
+		 super(inputName,inputSurname,inputPassportnumber);//this will call arg constructor from the person class
 		 setmatriculaNumber(inputMatriculanumber);
-		 setName(inputName);
-		 setsurName(inputSurname);
+		//setName(inputName);
+		// setsurName(inputSurname);
 		 setfaculty(inputFaculty);
 		 setBirthYear(inputBirthyear);
 		 setcountry(inputCountry);
-		 setpassportNumber(inputPassportnumber);
+		// setpassportNumber(inputPassportnumber);
 	     }
 	 
 	 //Favour Obidiaso (AB123456) ITF,1999
